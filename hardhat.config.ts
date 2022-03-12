054438c6c37213773744ab2894a61ff96e853d3b
Pavle Batuta <pavle@batuta.xyz>
2022-03-12 13:20:43 +0100

    Add original contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,11 @@
-import '@nomiclabs/hardhat-ethers'
-import '@nomiclabs/hardhat-etherscan'
-import '@nomiclabs/hardhat-waffle'
-import '@typechain/hardhat'
-import 'hardhat-contract-sizer'
-import { HardhatUserConfig } from 'hardhat/config'
-import { SolcUserConfig } from 'hardhat/types'
-import 'solidity-coverage'
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import 'hardhat-contract-sizer';
+import { HardhatUserConfig } from 'hardhat/config';
+import { SolcUserConfig } from 'hardhat/types';
+import 'solidity-coverage';
 
 const DEFAULT_COMPILER_SETTINGS: SolcUserConfig = {
   version: '0.5.17',
@@ -15,7 +15,7 @@ const DEFAULT_COMPILER_SETTINGS: SolcUserConfig = {
       runs: 1_000_000,
     },
   },
-}
+};
 
 // // NOTE: in case coverage breaks when using optimizer, this can help.
 // // See https://github.com/sc-forks/solidity-coverage/issues/417#issuecomment-730526466
@@ -69,21 +69,35 @@ const config: HardhatUserConfig = {
     },
   },
   solidity: {
-    compilers: [DEFAULT_COMPILER_SETTINGS],
+    compilers: [
+      DEFAULT_COMPILER_SETTINGS,
+      {
+        version: '0.8.12',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1_000_000,
+          },
+          metadata: {
+            bytecodeHash: 'none',
+          },
+        },
+      },
+    ],
   },
   contractSizer: {
     alphaSort: false,
     disambiguatePaths: true,
     runOnCompile: false,
   },
-}
+};
 
 if (process.env.ETHERSCAN_API_KEY) {
   config.etherscan = {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
     apiKey: process.env.ETHERSCAN_API_KEY,
-  }
+  };
 }
 
-export default config
+export default config;
